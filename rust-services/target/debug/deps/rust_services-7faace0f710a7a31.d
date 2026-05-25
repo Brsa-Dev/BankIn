e91488102ffab680786b5456f83c5ba40faa8957
Brsa.Dev <jordanbrsa.dev@gmail.com>
2026-05-25 16:20:59 +0200

    Ajout endpoints Nest

--- rust-services/target/debug/deps/rust_services-7faace0f710a7a31.d
+++
@@ -1,5 +0,0 @@
-G:\BankIn\rust-services\target\debug\deps\rust_services-7faace0f710a7a31.d: src\main.rs
-
-G:\BankIn\rust-services\target\debug\deps\librust_services-7faace0f710a7a31.rmeta: src\main.rs
-
-src\main.rs:
