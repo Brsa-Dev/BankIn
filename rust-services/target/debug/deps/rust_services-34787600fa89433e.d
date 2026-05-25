d96e127c718e1c90acdcda67bd5da7c3929fab76
JordanBRSA <jordan.barbosa-joal@toulouse.miage.fr>
2026-05-25 15:10:48 +0200

    1st commit

---
+++ rust-services/target/debug/deps/rust_services-34787600fa89433e.d
@@ -0,0 +1,5 @@
+G:\BankIn\rust-services\target\debug\deps\rust_services-34787600fa89433e.d: src\main.rs
+
+G:\BankIn\rust-services\target\debug\deps\librust_services-34787600fa89433e.rmeta: src\main.rs
+
+src\main.rs:
